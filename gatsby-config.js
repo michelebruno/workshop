c74cf8df27ba30dc09d67beb866bb05ed850d939
michelebruno <43304601+michelebruno@users.noreply.github.com>
2021-02-10 18:03:13 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  siteMetadata: {
+    title: "Workshop",
+  },
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
---
+++ src/pages/index.js
@@ -0,0 +1,23 @@
+import * as React from "react"
+import '../scss/style.scss'
+// markup
+const IndexPage = () => {
+  return (
+    <main >
+      <title>Home Page</title>
+      <h1>
+        Congratulations
+        <br />
+        <div className="w-100 bg-primary border border-dark border-3">
+          Hello
+        </div>
+        <span>— you just made a Gatsby site! </span>
+        <span role="img" aria-label="Party popper emojis">
+          🎉🎉🎉
+        </span>
+      </h1>
+    </main>
+  )
+}
+
+export default IndexPage
